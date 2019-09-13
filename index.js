2cfde7da3e1f9840a00576cc9642bd4b57a7a4ef
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2019-09-13 14:07:17 +0300

    chat: add simple chat sender

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /*jshint esversion: 6 */
 
+//io - server
+//socket - user
+
 let express = require('express');
 let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
 server.listen(3000);
+
+let usersCount = 0;
 
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
@@ -14,8 +19,13 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('connected');
+    socket.id = ++usersCount;
     socket.on('disconnect', function () {
         console.log('user disconnected');
+    });
+    socket.on('chat message',(msg) => {
+        console.log( "" + socket.id + " : " + msg);
+        io.emit('chat message', msg);
     });
 });
 
--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,18 @@
 /*jshint esversion: 6 */
+//import io from 'socket.io-client';
+let socket = io();
 
-let info = () => {
-    console.log("abrakadabra");
+// io.on('connection',(socket) => {
+//     socket.broadcast.emit('welcome message', 'I am here');
+// });
+
+document.querySelector('#button').onclick = () => {
+    socket.emit('chat message', document.querySelector('#message').value);
 };
 
-let socket = io();
+socket.on('chat message', (msg) => {
+    let li = document.createElement('li');
+    let ml = document.querySelector('#messages');
+    li.innerText = msg;
+    ml.appendChild(li);
+});
