be76f326ff657672763786e3cfc33113b9805f9f
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2019-09-16 21:38:32 +0300

    chat: add connection to DB

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 /*jshint esversion: 6 */
-
-//io - server
-//socket - user
-
 const help = require('./secondary.js');
 
 let express = require('express');
@@ -11,25 +7,37 @@ let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
 server.listen(3000);
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'chat';
+
+const doDatabase = (func,...params) => {
+    MongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        console.log('database connected');
+        const db = client.db(dbName);
+        func(db,...params);
+        client.close();
+    });
+};
+
+
+
 let messageStorage = [];
-
 let usersCount = 0;
-
-
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 app.use(express.static('public'));
 
-
 io.on('connection', (socket) => {
-
     socket.user = {};
     socket.user.id = ++usersCount;
     socket.user.username = 'guest';
     socket.user.color = help.generateColor();
-    socket.user.user = {};
 
     console.log('user#' + socket.user.id + ' connected');
     socket.emit('chat history', messageStorage);
@@ -40,7 +48,20 @@ io.on('connection', (socket) => {
 
     socket.on('chat message', (msg) => {
         console.log("" + socket.user.id + " : " + msg);
-        messageStorage.push([socket.user,msg]);
+        if (msg == "logIN") {
+            doDatabase(
+                help.logIn,
+                (res)=>{
+                    console.log("res: " + res);
+                    socket.user.username = res.info.username;
+                    socket.user.color = res.info.color;
+                },
+                'vbkoshak',
+                'vb12345'
+            );
+        }
+        messageStorage.push([socket.user, msg]);
+
         io.emit('chat message', socket.user, msg);
     });
 });
--- secondary.js
+++ secondary.js
@@ -1,9 +1,20 @@
 /*jshint esversion: 6 */
+const assert = require('assert');
 
 const generateColor = () => {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 };
 
+const logIn = (db,callback,login,password) => {
+    const userCollection = db.collection('users');
+    userCollection.find({login,password}).toArray(function(err,docs) {
+        assert.equal(err,null);
+        console.log("Found the folowing records: ");
+        console.log(docs);
+        callback(docs[0]);
+    });
+}
 
+module.exports.logIn = logIn;
 module.exports.generateColor = generateColor;
 
